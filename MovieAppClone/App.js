af73e4afcbebb049722e28f5a0651c4a3ea90da0
jher235 <127422627+jher235@users.noreply.github.com>
2023-05-02 22:45:19 +0900

    upload MovieAppClone

---
+++ MovieAppClone/App.js
@@ -0,0 +1,24 @@
+import {useState, useEffect } from "react";
+import Movie from "./components/Movie";
+import Detail from "./routes/Detail";
+import{
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Home from "./routes/Home"
+
+function App() {
+  return(<Router>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/movie/:num" element={<Detail />} />
+      <Route path="/hi" element ={<h1>hi</h1>} />
+    </Routes>
+  </Router>);
+}
+
+export default App;
+
---
+++ MovieAppClone/routes/Detail.js
@@ -0,0 +1,98 @@
+// import {useParams} from "react-router-dom"
+// import {useEffect, useState} from "react"
+// import Movie from "../components/Movie"
+// function Detail(){
+//     const {num} = useParams();
+//     const [loading,setLoading] = useState(true);
+//     const [movie,setMovie] = useState();
+//     const getMovies= async()=>{
+//         const json = await(
+//         await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${num}`)
+//       ).json();
+//         console.log(json.data.movie);
+        
+//         setMovie(json.data.movie);
+//         setLoading(false);
+//         //console.log(movie);
+//     };
+    
+//     useEffect(()=>{
+//       getMovies();
+       
+        
+//     },[])
+    
+//     useEffect(()=>{
+//         console.log(movie);}
+//         ,[movie])
+
+//     return <div>
+      
+//      {loading ? <h1>loading...</h1> : 
+//      <div>
+//         <Movie 
+//         key = {movie.id}
+//         id = {movie.id}
+//         mci = {movie.large_cover_image}
+//         title = {movie.title_long}
+//         genres = {movie.genres}
+//         url = {movie.url}
+//          />
+
+//     </div>
+//      }
+//     </div>
+// }
+
+
+
+// export default Detail;
+import {useParams} from "react-router-dom"
+import {useEffect, useState} from "react"
+import Movie from "../components/Movie"
+function Detail(){
+    const {num} = useParams();
+    const [loading,setLoading] = useState(true);
+    const [movie,setMovie] = useState();
+    const getMovies= async()=>{
+        const json = await(
+        await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${num}`)
+      ).json();
+        console.log(json.data.movie);
+        
+        setMovie(json.data.movie);
+        setLoading(false);
+        //console.log(movie);
+    };
+    
+    useEffect(()=>{
+      getMovies();
+       
+        
+    },[])
+    
+    useEffect(()=>{
+        console.log(movie);}
+        ,[movie])
+
+    return <div>
+      
+     {loading ? <h1>loading...</h1> : 
+     <div>
+        <Movie 
+        key = {movie.id}
+        id = {movie.id}
+        mci = {movie.large_cover_image}
+        title = {movie.title_long}
+        genres = {movie.genres}
+        url = {movie.url}
+         />
+
+    </div>
+     }
+    </div>
+}
+
+
+
+export default Detail;
